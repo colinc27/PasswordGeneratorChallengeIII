a0792e0b67ed4de67cd6d1aa18af6bda4678376c
Colin Christoph <colinchristoph27@gmail.com>
2022-02-23 15:56:38 -0700

    Added variables and window prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,24 @@
 // Assignment code here
-
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var symbol = "!@#$%^&*_-+=";
+var characterS = window.prompt('How many characters long would you like your password?');
+var upperQ = window.confirm("Would you like upper case letters?");
+var lowerQ = window.confirm("Would you like lower case letters?");
+var numberQ = window.confirm("Would you like numbers?");
+var specialQ = window.confirm("Would you like special characters?");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+   //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+ // passwordText.value = password;
 
 }
 
