914c63b5383e00ed5cf60c01d24df33f1de58895
Colin Christoph <colinchristoph27@gmail.com>
2022-02-26 23:42:06 -0700

    Print generated password and edit read me/clean up code

--- Develop/script.js
+++ Develop/script.js
@@ -10,18 +10,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-   var password = generate
-   var passwordText = document.querySelector("#password");
-   //Ask user about password criteria
+   //Ask user about password criteria and set up while loop for error
    var characterS = window.prompt('How many characters long would you like your password between 8 and 128 characters? Please enter your value as a number');
       while( characterS > 128 || characterS < 8 || isNaN(characterS)) {
          var characterS = window.prompt('ERROR PLEASE RESUBMIT!!\n How many characters long would you like your password between 8 and 128 characters? Please enter your value as a number');
       }
+   var lowerQ = window.confirm("Would you like your password to have lower case letters? (OK for yes, Cancel for no)");
    var upperQ = window.confirm("Would you like your password to have upper case letters? (OK for yes, Cancel for no)");
    var numberQ = window.confirm("Would you like your password to have numbers? (OK for yes, Cancel for no)");
    var specialQ = window.confirm("Would you like your password to have special characters? (OK for yes, Cancel for no)");
    //Setup string for the password based on the questions above
-   var characterAdd = lower;
+   var characterAdd = "";
+   if(lowerQ) characterAdd = characterAdd.concat(lower);
    if(upperQ) characterAdd = characterAdd.concat(upper);
    if(numberQ) characterAdd = characterAdd.concat(numbers);
    if(specialQ) characterAdd = characterAdd.concat(symbol);
@@ -31,10 +31,11 @@ function writePassword() {
       var passChar = characterAdd[Math.floor(Math.random()*characterAdd.length)]
       passwordText.push(String(passChar));
    }
+   //put password into text area
    var password = passwordText.join("");
-   console.log(passwordText);
-   console.log(password);
-}
+   document.getElementById('password').value=password;
+   }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
