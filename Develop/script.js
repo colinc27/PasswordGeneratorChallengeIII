0ae461cb59604638a6dc653b74ae1abf979987ae
Colin Christoph <colinchristoph27@gmail.com>
2022-02-26 21:33:09 -0700

    Add if statements and update to read me

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,6 @@ var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var symbol = "!@#$%^&*_-+=";
-var characterS = window.prompt('How many characters long would you like your password?');
-var upperQ = window.confirm("Would you like upper case letters?");
-var lowerQ = window.confirm("Would you like lower case letters?");
-var numberQ = window.confirm("Would you like numbers?");
-var specialQ = window.confirm("Would you like special characters?");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,8 +10,27 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-   //var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+   var password = generate
+   var passwordText = document.querySelector("#password");
+   //Ask questions pertaining to the password
+   var characterS = window.prompt('How many characters long would you like your password?');
+   var upperQ = window.confirm("Would you like your password to have upper case letters? (OK for yes, Cancel for no)");
+   var numberQ = window.confirm("Would you like your password to have numbers? (OK for yes, Cancel for no)");
+   var specialQ = window.confirm("Would you like your password to have special characters? (OK for yes, Cancel for no)");
+   var passwordOb = {
+      characterS,
+      upper,
+      lower, 
+      numbers,
+      symbol,
+   };
+   var characterAdd = lower;
+   if(upperQ) characterAdd = characterAdd.concat(upper);
+   if(numberQ) characterAdd = characterAdd.concat(numbers);
+   if(specialQ) characterAdd = characterAdd.concat(symbol);
+   console.log(characterAdd);
+   console.log(passwordOb)
+
 
  // passwordText.value = password;
 
