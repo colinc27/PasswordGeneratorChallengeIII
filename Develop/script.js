97a48ba72aa8ab70a67609b401d072278a7d32d4
Colin Christoph <colinchristoph27@gmail.com>
2022-02-26 22:09:42 -0700

    Add for loop for password generation

--- Develop/script.js
+++ Develop/script.js
@@ -12,28 +12,26 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
    var password = generate
    var passwordText = document.querySelector("#password");
-   //Ask questions pertaining to the password
+   //Ask user about password criteria
    var characterS = window.prompt('How many characters long would you like your password?');
    var upperQ = window.confirm("Would you like your password to have upper case letters? (OK for yes, Cancel for no)");
    var numberQ = window.confirm("Would you like your password to have numbers? (OK for yes, Cancel for no)");
    var specialQ = window.confirm("Would you like your password to have special characters? (OK for yes, Cancel for no)");
-   var passwordOb = {
-      characterS,
-      upper,
-      lower, 
-      numbers,
-      symbol,
-   };
+   //Setup string for the password based on the questions above
    var characterAdd = lower;
    if(upperQ) characterAdd = characterAdd.concat(upper);
    if(numberQ) characterAdd = characterAdd.concat(numbers);
    if(specialQ) characterAdd = characterAdd.concat(symbol);
    console.log(characterAdd);
-   console.log(passwordOb)
-
-
- // passwordText.value = password;
-
+   //setup for loop that crates password
+   var passwordText = [];
+   for(i = 0; i < characterS; i++){
+      var passChar = characterAdd[Math.floor(Math.random()*characterAdd.length)]
+      passwordText.push(String(passChar));
+   }
+   console.log(passwordText);
+   var password = passwordText.join("");
+   console.log(password)
 }
 
 // Add event listener to generate button
